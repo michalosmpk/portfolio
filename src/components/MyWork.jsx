91bdb5685a39dedc16c0ec71a962be492148d33d
michalosmpk <michalosmpk@gmail.com>
2024-01-31 02:32:05 +0100

    Added mobile functionality on MyWork

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -1,14 +1,59 @@
+import { useState, useRef, useEffect } from "react";
+
+import data from "../data/myWork.json";
 import "../scss/myWork.scss";
+import swipeHint from "../assets/swipeUp.svg";
 import code from "../assets/programistyczne_kody.png";
-import stenders from "../assets/work/stenders.png";
-import moonclimbing from "../assets/work/moonclimbing.png";
-import rareseeds from "../assets/work/rareseeds.png";
-import taxi from "../assets/work/taxi.png";
-import pastelove from "../assets/work/pastelove.png";
-import diamond from "../assets/work/diamond.png";
-import invicta from "../assets/work/invicta.png";
 
 function MyWork() {
+  const [isTileSwiped, setTileSwiped] = useState(false);
+  const startY = useRef([]);
+  const tileRefs = useRef([]);
+
+  const handleTouchStart = (e, index) => {
+    startY.current[index] = e.touches[0].clientY;
+  };
+
+  const handleTouchMove = (e, index) => {
+    if (startY.current[index] !== undefined && tileRefs.current[index]) {
+      const deltaY = e.touches[0].clientY - startY.current[index];
+
+      if (deltaY < -50) {
+        setTileSwiped(true);
+        tileRefs.current[index].classList.add("Work__TileMobileText-swiped");
+      } else if (deltaY > -50) {
+        tileRefs.current[index].classList.remove("Work__TileMobileText-swiped");
+      }
+    }
+  };
+
+  const handleTouchEnd = (index) => {
+    startY.current[index] = undefined;
+  };
+
+  useEffect(() => {
+    const handleTouchMoveWrapper = (e, index) => {
+      handleTouchMove(e, index);
+      e.preventDefault(); // Prevent the default scrolling behavior
+    };
+
+    tileRefs.current.forEach((tileRef, index) => {
+      tileRef.addEventListener(
+        "touchmove",
+        (e) => handleTouchMoveWrapper(e, index),
+        { passive: false }
+      );
+    });
+
+    return () => {
+      tileRefs.current.forEach((tileRef, index) => {
+        tileRef.removeEventListener("touchmove", (e) =>
+          handleTouchMoveWrapper(e, index)
+        );
+      });
+    };
+  }, []); // Run this effect once when the component mounts
+
   return (
     <section className="Work" id="MyWork">
       <div className="ContentWrapper">
@@ -18,125 +63,56 @@ function MyWork() {
         </h2>
 
         <div className="Work__Content">
-          <div className="Work__Grid">
-            <a
-              className="Work__Tile"
-              style={{ backgroundImage: `url('${stenders}')` }}
-              href="https://www.stenders-cosmetics.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="Work__TileBlur" />
-              <p className="Work__TileText">
-                Stenders Cosmetics
-                <p className="Work__TileTextAdditional">
-                  Worked on the frontend (React, Sass), SEO optimizations, bot
-                  rendering issues and GTM implementations.
-                </p>
-              </p>
-            </a>
+          <div className="Work__Grid Work__GridMobile">
+            {data.map((tile, index) => (
+              <div
+                key={tile.id}
+                className="Work__Tile"
+                style={{ backgroundImage: `url('${tile.image}')` }}
+              >
+                <div
+                  className="Work__TileMobileText"
+                  ref={(el) => el && tileRefs.current.push(el)}
+                  onTouchStart={(e) => handleTouchStart(e, index)}
+                  onTouchMove={(e) => handleTouchMove(e, index)}
+                  onTouchEnd={() => handleTouchEnd(index)}
+                >
+                  <span
+                    className={index === 0 && !isTileSwiped ? "text-left" : ""}
+                  >
+                    {tile.title}
+                  </span>
+                  {index === 0 && !isTileSwiped && (
+                    <img
+                      src={swipeHint}
+                      className="Work__TileMobileSwipeHint"
+                    />
+                  )}
+                  <p className="Work__TileMobileTextAdditional">
+                    {tile.content}
+                  </p>
+                  <a href={tile.link}>Proceed to website</a>
+                </div>
+              </div>
+            ))}
+          </div>
 
-            <a
-              className="Work__Tile"
-              style={{ backgroundImage: `url('${moonclimbing}')` }}
-              href="https://moonclimbing.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="Work__TileBlur" />
-              <p className="Work__TileText">
-                Moonclimbing
-                <p className="Work__TileTextAdditional">
-                  Worked on the frontend (AlpineJS, Tailwindcss) and backend
-                  (Magento 2) of the application.
+          <div className="Work__Grid Work__GridDesktop">
+            {data.map((tile, index) => (
+              <a
+                key={index}
+                className="Work__Tile"
+                style={{ backgroundImage: `url('${tile.image}')` }}
+                href={tile.link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <p className="Work__TileText">
+                  {tile.title}
+                  <p className="Work__TileTextAdditional">{tile.content}</p>
                 </p>
-              </p>
-            </a>
-
-            <a
-              className="Work__Tile"
-              style={{ backgroundImage: `url('${rareseeds}')` }}
-              href="https://www.rareseeds.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="Work__TileBlur" />
-              <p className="Work__TileText">
-                Rareseeds
-                <p className="Work__TileTextAdditional">
-                  Worked on the frontend (React, Sass) of the app.
-                </p>
-              </p>
-            </a>
-
-            <a
-              className="Work__Tile"
-              style={{ backgroundImage: `url('${taxi}')` }}
-              href="https://taxi29krasnystaw.pl"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="Work__TileBlur" />
-              <p className="Work__TileText">
-                Taxi 29 Krasnystaw
-                <p className="Work__TileTextAdditional">
-                  This is one of my older projects. It&apos;s surely not the
-                  most beautiful website I&apos;ve made however I&apos;ve
-                  learned a lot while developing it a few years back.
-                </p>
-              </p>
-            </a>
-
-            <a
-              className="Work__Tile"
-              style={{ backgroundImage: `url('${pastelove}')` }}
-              href="https://pastelove.pl/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="Work__TileBlur" />
-              <p className="Work__TileText">
-                Pastelove
-                <p className="Work__TileTextAdditional">
-                  Developed the whole frontend for the website. (Wordpress,
-                  Woocommerce)
-                </p>
-              </p>
-            </a>
-
-            <a
-              className="Work__Tile"
-              style={{ backgroundImage: `url('${diamond}')` }}
-              href="https://www.diamondmansion.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="Work__TileBlur" />
-              <p className="Work__TileText">
-                Diamond Mansion
-                <p className="Work__TileTextAdditional">
-                  Seo fixes, SEO optimizations, GTM implementation. (Magento 2,
-                  PWA Studio)
-                </p>
-              </p>
-            </a>
-
-            <a
-              className="Work__Tile"
-              style={{ backgroundImage: `url('${invicta}')` }}
-              href="https://invictastores.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="Work__TileBlur" />
-              <p className="Work__TileText">
-                Invicta
-                <p className="Work__TileTextAdditional">
-                  Seo fixes, SEO optimizations, GTM implementation, GA4
-                  measurement protocol implementation. (Magento 2, ScandiPWA)
-                </p>
-              </p>
-            </a>
+              </a>
+            ))}
           </div>
         </div>
       </div>
